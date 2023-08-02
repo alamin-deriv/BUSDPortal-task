08811abcbb327d5d8404ea8180112c7820a8688b
alamin-deriv <alamin.abdulkareem@flexisaf.com>
2023-08-02 18:22:57 +0100

    ---

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,11 +9,11 @@ module.exports = {
     localhost: {},
     goerli: {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: privateKeys.split(","),
+      accounts: privateKeys?.split(","),
     },
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: privateKeys.split(","),
+      accounts: privateKeys?.split(","),
     },
   },
 };
